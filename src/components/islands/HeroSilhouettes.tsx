27f8539d5b349a8bf12a9453d97a59e29ca00b6e
Junior Oliveira <juniordiasoli@gmail.com>
2025-11-11 17:59:02 +0000

    feat: layout fixes

--- src/components/islands/HeroSilhouettes.tsx
+++ src/components/islands/HeroSilhouettes.tsx
@@ -4,8 +4,8 @@ import { Logo } from './Logo';
 export const HeroSilhouettes: React.FC = () => {
   return (
     <>
-      <div className="absolute top-0 -right-32 -translate-y-1/2 opacity-60 grayscale brightness-0 z-49">
-        <Logo className="w-64 h-64" />
+      <div className="absolute top-0 -translate-y-1/2 opacity-60 grayscale brightness-0 z-49 right-0 md:-right-32 overflow-hidden">
+        <Logo className="w-32 h-32 md:w-64 md:h-64" />
       </div>
     </>
   );
