43155b6ce2fc2e11d4d853d60b071993c872ccec
Junior Oliveira <juniordiasoli@gmail.com>
2025-11-12 00:24:07 +0000

    refactoring: some refactorings on the code structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { SimpleLoader } from './islands/SimpleLoader.tsx';
 
 export { default as Header } from './layout/Header.astro';
 export { default as Footer } from './layout/Footer.astro';
+export { default as PageContainer } from './layout/PageContainer.astro';
 
 export { default as BlogCard } from './content/BlogCard.astro';
 export { default as ProjectCard } from './content/ProjectCard.astro';
@@ -12,3 +13,8 @@ export { default as ProjectCard } from './content/ProjectCard.astro';
 export { default as HeroSection } from './sections/HeroSection.astro';
 export { default as LatestArticlesSection } from './sections/LatestArticlesSection.astro';
 export { default as TechStackSection } from './sections/TechStackSection.astro';
+
+export { default as Icon } from './ui/Icon.astro';
+export { default as StatsDisplay } from './ui/StatsDisplay.astro';
+export { default as BackButton } from './ui/BackButton.astro';
+export { default as CTAButton } from './ui/CTAButton.astro';
